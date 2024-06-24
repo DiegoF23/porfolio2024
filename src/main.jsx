58e985cf6119a1d04a9b7736156f05899144100a
flores diego <floresdiego2012@gmail.com>
2024-06-24 18:15:13 -0300

    inicio de porfolio

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Porfolio from './Porfolio.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Porfolio />
+  </React.StrictMode>,
+)
