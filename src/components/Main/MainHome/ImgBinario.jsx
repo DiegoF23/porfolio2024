ef33cd7e3e545d0338be6740b6f19c3cf720f157
flores diego <floresdiego2012@gmail.com>
2024-06-28 21:45:53 -0300

    add imgPerfil

--- src/components/Main/MainHome/ImgBinario.jsx
+++ src/components/Main/MainHome/ImgBinario.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import '../../../css/Main/MainInicio/ImgBinario.css'
-import PerfilImg from '../../../assets/images/imgPerfil.png'
+import PerfilImg from '../../../assets/images/imgPerfil.svg'
 const ImgBinario = () => {
   return (
     <>
