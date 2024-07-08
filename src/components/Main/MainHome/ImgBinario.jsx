408c329d1b96740c950f3f2e30e4f8f8fa35e341
MavyAbraham <abrahammavy6@gmail.com>
2024-07-07 22:56:45 -0300

    Change of MainHome image

--- src/components/Main/MainHome/ImgBinario.jsx
+++ src/components/Main/MainHome/ImgBinario.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import '../../../css/Main/MainInicio/ImgBinario.css'
-import PerfilImg from '../../../assets/images/imgPerfil.svg'
+import PerfilImg from '../../../assets/images/imgPefil2.png'
 const ImgBinario = () => {
   return (
     <>
