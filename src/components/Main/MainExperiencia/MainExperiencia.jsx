73c99748ddf6b6ce441c07d63bbc4ca5366ab1ac
MavyAbraham <abrahammavy6@gmail.com>
2024-07-15 19:32:48 -0300

    Half done Experiencia

--- src/components/Main/MainExperiencia/MainExperiencia.jsx
+++ src/components/Main/MainExperiencia/MainExperiencia.jsx
@@ -6,20 +6,31 @@ import "../../../css/Main/MainExperiencia/MainExperiencia.css"
 const MainExperiencia = () => {
   return (
     <div className="MainExperiencia">
+
       <div className="ContenedorSlider">
-      <div className="slider">
-        <IconBar />
+        <div className="slider">
+          <IconBar />
+        </div>
       </div>
-      </div>
-      
-      <h1>Experiencias</h1>
+
       <div className="Experiencias">
-        {Experiencias.map((experiencia, index) => (
-          <div className="boxExperiencia" key={index}>
-            <h4> {experiencia.titulo} </h4>
-            <p> {experiencia.texto} </p>
-          </div>
-        ))}
+        <h1>Experiencias</h1>
+        
+
+        <div className="contenedorBEXP" >
+
+          {Experiencias.map((experiencia, index) => (
+            <div className="line">
+              <div key={index} className="boxExperiencia" >
+                <h4> {experiencia.titulo} </h4>
+                <p> {experiencia.texto} </p>
+              </div>
+
+            </div>
+          ))}
+
+        </div>
+
       </div>
     </div>
   );
